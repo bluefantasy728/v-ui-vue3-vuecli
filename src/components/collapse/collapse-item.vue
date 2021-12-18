4c7e645ed81c6aea780574f498b2b55b853f1d1c
bluefantasy728 <jiangguoxi728@hotmail.com>
2021-12-18 14:33:59 +0800

    添加collapse组件

---
+++ src/components/collapse/collapse-item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-collapse-item"></div>
+</template>
+<script>
+export default {
+  name: 'v-collapse-item',
+}
+</script>
+<script setup>
+const props = defineProps({})
+</script>
+
+<style scoped lang="scss">
+@use 'collapse-item.scss';
+</style>
---
+++ src/components/collapse/collapse.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="[
+    'v-collapse',
+    
+  ]"></div>
+</template>
+<script>
+export default {
+  name: 'v-collapse',
+}
+</script>
+<script setup>
+const props = defineProps({})
+</script>
+
+<style scoped lang="scss">
+@use 'collapse.scss';
+</style>
